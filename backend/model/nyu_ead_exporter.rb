3eaf6f295f76f7169d16a150a2669eb25622ec6d
Esha Datta <eshadatta16@gmail.com>
2016-05-19 06:07:23 -0400

    removed id attribute from container element if container contains a parent attribute

--- backend/model/nyu_ead_exporter.rb
+++ backend/model/nyu_ead_exporter.rb
@@ -155,6 +155,33 @@ class EADSerializer < ASpaceExport::Serializer
   def customize_ead_data(custom_text,data)
     custom_text + data
   end
+
+  def serialize_container(inst, xml, fragments)
+    containers = []
+    @parent_id = nil
+    (1..3).each do |n|
+      atts = {}
+      next unless inst['container'].has_key?("type_#{n}") && inst['container'].has_key?("indicator_#{n}")
+      @container_id = prefix_id(SecureRandom.hex)
+
+      atts[:parent] = @parent_id unless @parent_id.nil?
+      atts[:id] = @container_id unless atts[:parent]
+      @parent_id = @container_id
+
+      atts[:type] = inst['container']["type_#{n}"]
+      text = inst['container']["indicator_#{n}"]
+      if n == 1 && inst['instance_type']
+        atts[:label] = I18n.t("enumerations.instance_instance_type.#{inst['instance_type']}", :default => inst['instance_type'])
+        if inst['container']["barcode_1"]
+          atts[:label] << " (#{inst['container']['barcode_1']})"
+        end
+      end
+      xml.container(atts) {
+         sanitize_mixed_content(text, xml, fragments)
+      }
+    end
+  end
+
   def serialize_eadheader(data, xml, fragments)
     eadheader_atts = {:findaidstatus => data.finding_aid_status,
                       :repositoryencoding => "iso15511",
