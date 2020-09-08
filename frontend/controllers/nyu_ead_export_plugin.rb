1eda2630abf9f042f3cc1bf301d236dca756a581
Don Mennerich <don.mennerich@nyu.edu>
2020-09-08 16:26:28 -0400

    adding variables

--- frontend/controllers/nyu_ead_export_plugin.rb
+++ frontend/controllers/nyu_ead_export_plugin.rb
@@ -3,9 +3,6 @@ class NyuEadExportPluginController < ApplicationController
   skip_before_action :unauthorised_access
 
   def index
-    version = "2.8.0"
-    url = "https://github.com/NYULibraries/nyu_ead_export_plugin/releases/tag/v2.7.1"
-    maintainer = "Donald Mennerich don.mennerich@nyu.edu"
   end
 
 end
